62ff3e776d60f5cb1387add685a8549ffe875738
Vasyl Smutko <Curare16071969@gmail.com>
2022-11-02 03:26:18 +0300

    2nd task

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryPhoto = document.querySelector(".gallery");
+const items = galleryItems;
 
-console.log(galleryItems);
+function createGalleryItems(items) {
+    return items.map((item) => `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`).join("");
+}
+
+
+galleryPhoto.innerHTML = createGalleryItems(galleryItems);
+const lightbox = new SimpleLightbox(".gallery__item", { captionsData: "alt" , captionDelay: 250} );
+
+// const addEls = createGalleryItems(galleryItems);
+// galleryPhoto.innerHTML = addEls;
+
+
+// galleryPhoto.addEventListener('click', imageLoop);
+
+// function imageLoop(event) { 
+//     event.preventDefault();
+//     if (event.target === galleryPhoto) {
+//         return;
+//     }
+    
+    
+// }
